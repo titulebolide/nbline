76b8b7e3673b299b290f4305008f5264a988beaf
titulebolide <seneztit@gmail.com>
2021-03-07 12:08:49 +0100

    add git option to run through a git repo

---
+++ conf-default.py
@@ -0,0 +1,3 @@
+included_extensions = ['py', 'js', 'php', 'html', 'css', 'c', 'h', 'cpp']
+excluded_folder = ['.git', 'build', 'node_modules', 'docs', '__pycache__', 'venv', 'site-packages', 'dropzone']
+excluded_files = []
